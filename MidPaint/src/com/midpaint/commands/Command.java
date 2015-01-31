902bab6870ddea483e4881655195dfe6aa238bf5
android <android@android>
2015-01-31 19:32:07 +0800

    Added Command Pattern for DrawShape

---
+++ MidPaint/src/com/midpaint/commands/Command.java
@@ -0,0 +1,22 @@
+/*
+ *  ==++++++++++++++++++++++++++++++++++++++++++++++++++++==
+ *  |      CENTRAL PHILIPPINE UNIVERSITY                   |
+ *  |      Bachelor of Science in Software Engineering     |
+ *  |      Jaro, Iloilo City, Philippines                  |
+ *  |                                                      |
+ *  |          This program is written by Rave, ©2015.     |
+ *  |          You are free to use and distribute this.    |
+ *  |          Reach me at: admin@blackout.biz.tm          |
+ *  |                                                      |
+ *  |               ~~~"CODE the FUTURE"~~~                |
+ *  ==++++++++++++++++++++++++++++++++++++++++++++++++++++==
+ */
+package com.midpaint.commands;
+
+/**
+ *
+ * @author Rave Noren Gidor-Sambo Villavicencio-Arevalo
+ */
+public interface Command{
+    public void execute();    
+}
---
+++ MidPaint/src/com/midpaint/commands/DrawShapeCommand.java
@@ -0,0 +1,43 @@
+/*
+ *  ==++++++++++++++++++++++++++++++++++++++++++++++++++++==
+ *  |      CENTRAL PHILIPPINE UNIVERSITY                   |
+ *  |      Bachelor of Science in Software Engineering     |
+ *  |      Jaro, Iloilo City, Philippines                  |
+ *  |                                                      |
+ *  |          This program is written by Rave, ©2015.     |
+ *  |          You are free to use and distribute this.    |
+ *  |          Reach me at: admin@blackout.biz.tm          |
+ *  |                                                      |
+ *  |               ~~~"CODE the FUTURE"~~~                |
+ *  ==++++++++++++++++++++++++++++++++++++++++++++++++++++==
+ */
+package com.midpaint.commands;
+
+import com.midpaint.objects.Canvas;
+import com.midpaint.objects.Shape;
+
+/**
+ *
+ * @author Rave Noren Gidor-Sambo Villavicencio-Arevalo
+ */
+public class DrawShapeCommand implements Command, Cloneable {
+
+    private Shape shape;
+    private Canvas canvas;
+
+    public DrawShapeCommand(Shape shape, Canvas canvas) {
+        this.shape = shape;
+        this.canvas = canvas;
+    }       
+    
+    @Override
+    public void execute() {
+        this.canvas.addShape(shape);
+    }
+    
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+    
+}
